63aea5e528b6e29144241e83335586d77eb55ab1
SalmanGit87 <salman.tariq.87@gmail.com>
2023-08-18 17:15:31 +0500

    variables

--- variable.tf
+++ variable.tf
@@ -1,3 +1,4 @@
+#plain variables
 variable "fruit_name" {
   default = "apple"
 }
@@ -5,3 +6,10 @@ variable "fruit_name" {
 output "fruit_name" {
   value = var.fruit_name
 }
+
+#list variables
+variable "cars" {
+  default = [1,2,3,"four"]
+}
+output "cars" {
+  value = var.cars}
